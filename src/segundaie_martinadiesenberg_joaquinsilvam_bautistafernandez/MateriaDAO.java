151c08a4bcfce5f7ef199bad12940d9a07f4e3e5
Alumno <Alumno@PC28>
2026-06-05 10:28:52 -0300

    agregue daoy controller

---
+++ src/segundaie_martinadiesenberg_joaquinsilvam_bautistafernandez/MateriaDAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package segundaie_martinadiesenberg_joaquinsilvam_bautistafernandez;
+
+/**
+ *
+ * @author Alumno
+ */
+public class MateriaDAO {
+    
+}
